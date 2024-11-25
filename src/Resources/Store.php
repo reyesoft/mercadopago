e6d025c6cd3d488bc50e8b0d33e151dd8000c374
Pablo Reyes <pablo@reyesoft.com>
2024-11-25 13:15:16 -0300

    v1_to_v3 auto-commit

---
+++ src/Resources/Store.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright (C) 1997-2020 Reyesoft <info@reyesoft.com>.
+ *
+ * This file is part of CryptoQr. CryptoQr can not be copied and/or
+ * distributed without the express permission of Reyesoft
+ */
+
+declare(strict_types=1);
+
+namespace MercadoPagoQr\Resources;
+
+class Store
+{
+    public ?int $id;
+    public ?string $name;
+    public ?string $date_created;
+    public ?array $business_hours;
+    public ?array $location;
+    public ?string $external_id;
+}
